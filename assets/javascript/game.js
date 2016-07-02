39b0b8345705673d5fb60ba36ab84dc8da9a5a5c
Todd Germaine <trgermaine@gmail.com>
2016-07-01 21:31:40 -0400

    updated javascript to make letters appear

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,41 +2,59 @@ window.onload = function () {
 
 //Global Variables
 
-var userGuess = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-
 var wins = 0;
 var losses = 0;
 var guesses = 6;
+var lettersGuessed = [];
 
 var words = ["NICOLE", "TODD", "DEBORAH", "KEITH", "TAYLOR"];
 
-//Shows Current Wins & Losses
+//Shows Current Wins & Losses and appends to page
 
 document.querySelector('#wins').innerHTML = "Wins: " + wins;
 document.querySelector('#losses').innerHTML = "Losses: " + losses;
+document.querySelector('.guessedLetters').innerHTML = "Letters Guessed: " + lettersGuessed;
 
-
+// 
 
 document.onkeyup = function(event) {
   
   var keypress = String.fromCharCode(event.keyCode).toLowerCase();
-	console.log(keypress);  
+
+	lettersGuessed.push(keypress + "  ");
+		console.log(lettersGuessed);
+
+	document.querySelector('#guesses').innerHTML = "Guesses: " + guesses;
 	guesses--;
+
+	// console.log(userGuess);
+	// Pushes typed letters on guessed letters div
+	document.querySelector(".guessedLetters").innerHTML = "Letters Guessed: " + "<br>" + lettersGuessed;
+	if (guesses <= -1) {
+
+		alert('You Lost!');
+		reset();
+	} else {
+
+
+	}
+
+	// document.querySelector(".lettersGuessed").innerHTML = guessedLetters.join(" ");
+	// document.getElementById('#guessed').appendChild(keypress);
+	// kepress.push(guessedLetters);
+	// console.log(guessedLetters[i]);
 };
-
-
-document.querySelector('#guessed').innerHTML = keypress;
 
 //Shows userGuess and appends it to the Letters Guessed Div
 
 
 function reset() {
 
+	window.location.href = "index.html";
 	var guesses = 6;
 
 }
 
-reset();
 
 
 }
