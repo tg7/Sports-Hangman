e598ecdaa3431333109dbf3d11d0097421e7a092
Todd Germaine <trgermaine@gmail.com>
2016-07-13 16:14:28 -0400

    edited wins, losses, and guesses categories

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
 window.onload = function () {
 
 //Global Variables
-var wins;
-var losses;
+var wins = 0;
+var losses = 0;
 var lettersGuessed;
 var boxes;
 var words = ["METS", "PHILLIES", "DODGERS", "YANKEES"];
 var answer;
-var guesses;
+var guesses = 8;
 var start = null;
 
 //ID Element Variables
@@ -17,16 +17,19 @@ var switchWins = document.getElementById('wins');
 var switchLosses = document.getElementById('losses');
 var switchGuesses = document.getElementById('guesses');
 
+// Start Of Functions
 
 function makeBoxes() {
+    guesses = 8;
 	for (var i = 0; i < answer.length; i++) {
-		boxes.push('_');
-		switchBoxes.innerHTML = "The Word Is: " + boxes;
+		boxes.push(' ___ ');
+    console.log(boxes);
+		switchBoxes.innerHTML = "The Word Is: " + boxes.join('');
     switchLetters.innerHTML = "Letters Guessed: ";
-    wins = switchWins.innerHTML;
-    losses = switchLosses.innerHTML;
+    switchWins.innerHTML = "Wins: " + wins;
+    switchLosses.innerHTML = "Losses: " + losses;
+    switchGuesses.innerHTML = "Guesses: " + guesses;
 	}
- 
 }
 
 function startGame() {
@@ -35,8 +38,6 @@ function startGame() {
   answer = words[Math.floor(Math.random() * words.length)];
   console.log('The Word Is: ' + answer);
   makeBoxes();
-  guesses = 8;
-
 }
 
 // Calls makeBoxes Function
@@ -45,10 +46,12 @@ startGame();
 //Keypress Function
 document.onkeyup = function(event) {
   
-  var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
-
   // Subtracts guess on each letter pressed
   guesses--;
+  // Assigns new value to guesses div id on page
+  switchGuesses.innerHTML = "Guesses: " + guesses;
+
+  var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
 	
 	// Pushes userGuess to lettersGuessed Array
   lettersGuessed.push(userGuess);
@@ -63,15 +66,12 @@ document.onkeyup = function(event) {
   console.log(userGuess);
 
   //Pushes pressed key to the letters guessed array
-  
+    
   for (var i = 0; i < answer.length; i++) {
   		
 		  if (userGuess === answer[i]) {
 		  	boxes[i] = answer[i];
-		  	// joinedAnswer = boxes.join('_');
-		  	switchBoxes.innerHTML = "The Word Is: " + boxes;
-		  	// console.log('You guessed right!');
-
+		  	switchBoxes.innerHTML = "The Word Is: " + boxes.join('');
 		  } 
 		  
 		  var completeWord = boxes.join('')
